585bc9774eba2df1e58f0bb3261306875a64cc05
Yeannot <imele.jm@gmail.com>
2015-12-10 18:27:22 +0100

    Uptdate convertNumberToString.py

--- convertNumberToString.py
+++ convertNumberToString.py
@@ -11,18 +11,18 @@ def numToStr(number):
     
     while strNum[0] == "0":
         strNum = strNum[1:]
-            
+        
     if len(strNum) > 2:
         firstNum  = int(strNum[0])
         secondNum = int(strNum[1:3])
         result = numbers[firstNum] + " " + hundred + " " + numbers[secondNum]
     else:
         result = numbers[number]
-    
-
+        
+        
     return result.replace(" zero", "")
-
-#    
+    
+#
 def convertNumber():
     number = raw_input("Type number :")
     result = numToStr(number)
